269eca583c51acf1e8f16d91e113b266114ae882
Nupur Makwana <makwananupur@gmail.com>
2026-05-03 10:55:07 +0530

    modified:   config/FirebaseConfig.js

--- config/FirebaseConfig.js
+++ config/FirebaseConfig.js
@@ -7,15 +7,16 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: "ai-fusion-nupur-makwana.firebaseapp.com",
-  projectId: "ai-fusion-nupur-makwana",
-  storageBucket: "ai-fusion-nupur-makwana.firebasestorage.app",
-  messagingSenderId: "762774722926",
-  appId: "1:762774722926:web:2a0a555f555c76f580497f",
-  measurementId: "G-GQHB0XW2CC"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app,'default')
+export const db = getFirestore(app)
